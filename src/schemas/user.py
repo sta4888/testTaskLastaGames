f0674e9132d3dc14e6a18bb0289704154e48491e
Eugene <talonin92@mail.ru>
2025-06-14 08:50:32 +0500

    v0.0.9.12 - user +

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -4,11 +4,3 @@ from pydantic import BaseModel
 class UserCreate(BaseModel):
     username: str
     password: str
-
-class UserLogin(BaseModel):
-    username: str
-    password: str
-
-class ChangePassword(BaseModel):
-    old_password: str
-    new_password: str
