866574f4b500ce0d931e60078db4385e34b829b9
Eugene <talonin92@mail.ru>
2025-06-14 08:47:15 +0500

    v0.0.9.11 - Document +

--- src/api/v2/endpoints/documents.py
+++ src/api/v2/endpoints/documents.py
@@ -50,7 +50,7 @@ def get_statistics(document_id: int, current_user=Depends(UserService.get_curren
     if not doc or doc.owner_id != current_user.id:
         raise HTTPException(status_code=404, detail="Document not found")
     stats = stat_repo.get_by_file_id(doc.file_id)
-    return [{"word": s.term, "tf": s.tf, "idf": s.idf} for s in stats]  # здесь так, так как в модели нет word
+    return [TermResponse(word=s.term, tf=s.tf, idf=s.idf) for s in stats]
 
 
 @router.delete("/{document_id}", response_model=MessageResponse, summary="удалить документ")
@@ -61,4 +61,4 @@ def delete_document(document_id: int, current_user=Depends(UserService.get_curre
     if not doc or doc.owner_id != current_user.id:
         raise HTTPException(status_code=404, detail="Document not found")
     repo.delete(document_id)
-    return {"message": "Document deleted"}
+    return MessageResponse(message="Document deleted")
